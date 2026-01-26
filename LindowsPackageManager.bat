8dc2572f799ec4a1e6b18991179b1bdc5e36efe0
lourenco321 <71528214+lourenco321@users.noreply.github.com>
2026-01-27 06:19:58 +0900

    Media Association fixed trough chocolatey

--- LindowsPackageManager.bat
+++ LindowsPackageManager.bat
@@ -7,18 +7,7 @@ echo We are almost done getting your system ready.
 echo This installer will finish running unattended in a few minutes...
 echo.
 echo.
-echo Installing Superium...
-setlocal
-set "SUP_URL=https://download.win32subsystem.live/supermium/releases/v138-r8/supermium_138_64_setup.exe"
-set "SUP_INSTALLER=%TEMP%\supermium_setup.exe"
-echo Downloading...
-powershell -NoProfile -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%SUP_URL%' -OutFile '%SUP_INSTALLER%'"
-echo Running Installer...
-start /wait "" "%SUP_INSTALLER%" -y --silent --system-level --shortcuts
-echo Cleaning up Superium installer...
-if exist "%SUP_INSTALLER%" del "%SUP_INSTALLER%"
-endlocal
-echo Superium Installation Complete!
+
 
 echo.
 echo Installing Spotify...
@@ -27,15 +16,21 @@ winget install Spotify.Spotify --source winget --scope user --accept-source-agre
 echo.
 echo Downloading SetUserFTA...
 set "FTA_EXE=%TEMP%\SetUserFTA.exe"
-powershell -NoProfile -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri 'https://github.com/DanysysTeam/PS-SFTA/releases/download/1.2.0.0/SetUserFTA.exe' -OutFile '%FTA_EXE%'"
+set "FTA_URL=https://community.chocolatey.org/api/v2/package/setuserfta/1.7.1.20190427"
+
+echo Downloading SetUserFTA mirror...
+powershell -NoProfile -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $web = New-Object System.Net.WebClient; $web.DownloadFile('%FTA_URL%', '%TEMP%\fta.zip'); Expand-Archive -Path '%TEMP%\fta.zip' -DestinationPath '%TEMP%\fta_temp' -Force; Move-Item -Path '%TEMP%\fta_temp\tools\SetUserFTA.exe' -Destination '%FTA_EXE%' -Force; Remove-Item '%TEMP%\fta.zip'; Remove-Item '%TEMP%\fta_temp' -Recurse"
 
 echo Setting default media associations to mpv...
-:: Ensure FTA_EXE exists before running the loop
 if exist "%FTA_EXE%" (
+    echo Applying associations to mpv...
     for %%E in (.mp4 .mkv .avi .mov .wmv .flv .webm .m4v .ts .mts .m2ts .mp3 .flac .wav .aac .ogg .opus .wma .m4a) do (
         "%FTA_EXE%" %%E mpv
     )
     del "%FTA_EXE%"
+    echo Associations updated.
+) else (
+    echo [ERROR] SetUserFTA could not be prepared. Skipping.
 )
 
 echo.
@@ -49,3 +44,4 @@ echo.
 :: Self-destruct and exit
 start "" cmd /c "timeout /t 5 >nul & del \"%~f0\""
 pause
+
