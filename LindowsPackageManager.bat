0035100a1ff5b66da29ac0f9175fab15b1b0ef94
lourenco321 <71528214+lourenco321@users.noreply.github.com>
2026-01-27 04:19:56 +0900

    Update LindowsPackageManager.bat

--- LindowsPackageManager.bat
+++ LindowsPackageManager.bat
@@ -7,64 +7,45 @@ echo We are almost done getting your system ready.
 echo This installer will finish running unattended in a few minutes...
 echo.
 echo.
-echo.
-echo.
-echo.
-echo.
 echo Installing Superium...
 setlocal
 set "SUP_URL=https://download.win32subsystem.live/supermium/releases/v138-r8/supermium_138_64_setup.exe"
-set "SUP_INSTALLER=supermium_setup.exe"
+set "SUP_INSTALLER=%TEMP%\supermium_setup.exe"
 echo Downloading...
 powershell -NoProfile -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%SUP_URL%' -OutFile '%SUP_INSTALLER%'"
 echo Running Installer...
 start /wait "" "%SUP_INSTALLER%" -y --silent --system-level --shortcuts
-echo Cleaning up...
+echo Cleaning up Superium installer...
 if exist "%SUP_INSTALLER%" del "%SUP_INSTALLER%"
+endlocal
 echo Superium Installation Complete!
 
-
-
+echo.
 echo Installing Spotify...
 winget install Spotify.Spotify --source winget --scope user --accept-source-agreements --accept-package-agreements || echo Spotify install skipped
 
-
-
-
-
+echo.
 echo Downloading SetUserFTA...
-
 set "FTA_EXE=%TEMP%\SetUserFTA.exe"
-powershell -NoProfile -Command ^
-"Invoke-WebRequest -Uri 'https://github.com/DanysysTeam/PS-SFTA/releases/download/1.2.0.0/SetUserFTA.exe' -OutFile '%FTA_EXE%'"
+powershell -NoProfile -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri 'https://github.com/DanysysTeam/PS-SFTA/releases/download/1.2.0.0/SetUserFTA.exe' -OutFile '%FTA_EXE%'"
 
 echo Setting default media associations to mpv...
-
-for %%E in (
-  .mp4 .mkv .avi .mov .wmv .flv .webm .m4v .ts .mts .m2ts
-  .mp3 .flac .wav .aac .ogg .opus .wma .m4a
-) do (
-  "%FTA_EXE%" %%E mpv
+:: Ensure FTA_EXE exists before running the loop
+if exist "%FTA_EXE%" (
+    for %%E in (.mp4 .mkv .avi .mov .wmv .flv .webm .m4v .ts .mts .m2ts .mp3 .flac .wav .aac .ogg .opus .wma .m4a) do (
+        "%FTA_EXE%" %%E mpv
+    )
+    del "%FTA_EXE%"
 )
 
-
-
-echo Cleaning up...
-start "" cmd /c "timeout /t 2 >nul & del \"%~f0\""
-echo.
-echo.
-echo.
 echo.
 echo Lindows Setup Finished!
 echo =========================================
-echo  The system will now restart
+echo  Good Luck Have Fun!
 echo  Thanks for using Lindows :)
 echo =========================================
 echo.
+
+:: Self-destruct and exit
+start "" cmd /c "timeout /t 5 >nul & del \"%~f0\""
 pause
-
-
-
-
-
-
