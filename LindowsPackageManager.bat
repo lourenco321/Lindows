2fbaa2f4a5aa604a92274921b575fd2361e0eb75
lourenco321 <71528214+lourenco321@users.noreply.github.com>
2026-01-27 06:29:40 +0900

    Switch to SFTA
    Thanks Danny!

--- LindowsPackageManager.bat
+++ LindowsPackageManager.bat
@@ -14,24 +14,13 @@ echo Installing Spotify...
 winget install Spotify.Spotify --source winget --scope user --accept-source-agreements --accept-package-agreements || echo Spotify install skipped
 
 echo.
-echo Downloading SetUserFTA...
-set "FTA_EXE=%TEMP%\SetUserFTA.exe"
-set "FTA_URL=https://raw.githubusercontent.com/qis/windows/master/setup/SetUserFTA/SetUserFTA.exe"
-
-echo Downloading SetUserFTA mirror...
-powershell -NoProfile -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $web = New-Object System.Net.WebClient; $web.Headers.Add('User-Agent', 'Mozilla/5.0'); $web.DownloadFile('%FTA_URL%', '%FTA_EXE%')"
-
 echo Setting default media associations to mpv...
-if exist "%FTA_EXE%" (
-    echo Applying associations to mpv...
-    for %%E in (.mp4 .mkv .avi .mov .wmv .flv .webm .m4v .ts .mts .m2ts .mp3 .flac .wav .aac .ogg .opus .wma .m4a) do (
-        "%FTA_EXE%" %%E mpv
-    )
-    del "%FTA_EXE%"
-    echo Associations updated.
-) else (
-    echo [ERROR] SetUserFTA could not be prepared. Skipping.
-)
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+    "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; " ^
+    "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/DanysysTeam/PS-SFTA/master/SFTA.ps1')); " ^
+    "$exts = '.mp4','.mkv','.avi','.mov','.wmv','.flv','.webm','.m4v','.ts','.mts','.m2ts','.mp3','.flac','.wav','.aac','.ogg','.opus','.wma','.m4a'; " ^
+    "foreach ($ext in $exts) { Set-FTA 'mpv' $ext }"
+
 
 echo.
 echo Lindows Setup Finished!
@@ -44,5 +33,6 @@ echo.
 :: Self-destruct and exit
 start "" cmd /c "timeout /t 5 >nul & del \"%~f0\""
 pause
+
 
 
